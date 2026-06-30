c80c5e6760ea5e85de6867203a3437d1719b3c1a
heelstriker <takehiro.ito@gmail.com>
2026-06-30 10:35:16 -0700

    fixing error

--- services/db.py
+++ services/db.py
@@ -3,8 +3,9 @@ import sqlite3
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-DB_DIR = os.path.join(BASE_DIR, "..", "database")
+DB_DIR = os.path.join(BASE_DIR, "database")
 DB_PATH = os.path.join(DB_DIR, "inventory.db")
+
 
 os.makedirs(DB_DIR, exist_ok=True)
 
