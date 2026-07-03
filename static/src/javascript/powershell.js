7b7157312a1cba99382b4265030ea913e694984a
heelstriker <takehiro.ito@gmail.com>
2026-07-03 13:29:30 -0700

    javascript for powershell update

--- static/src/javascript/powershell.js
+++ static/src/javascript/powershell.js
@@ -66,10 +66,47 @@ A    85       415      FileSystem   \\\\NYCSERVER01\\Accounting
 H    42       258      FileSystem   \\\\LAXSERVER01\\HR_Shared
 `
  
-                               
+                             
+
+testconnection:
+
+`Windows PowerShell
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+PS C:\\Users\\Administrator> Test-Connection LAXSERVER01
+
+Source        Destination      IPV4Address       Bytes    Time(ms)
+-----------------------------------------------------------------------
+LAXACTPC001   LAXSERVER01      192.168.100.10    32       1
+LAXACTPC001   LAXSERVER01      192.168.100.10    32       1
+LAXACTPC001   LAXSERVER01      192.168.100.10    32       2
+LAXACTPC001   LAXSERVER01      192.168.100.10    32       1
+`
+
+
+hotfix:
+
+`Windows PowerShell
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+PS C:\\Users\\Administrator> Get-HotFix
+
+Source Description HotFixID InstalledOn
+---------------------------------------------------------------
+LAXACTPC001 Update KB5062553 07/02/2026
+LAXACTPC001 Security Update KB5061007 06/12/2026
+LAXACTPC001 Update KB5059806 05/18/2026
+LAXACTPC001 Update KB5056579 04/09/2026
+`
+
+
+
+  
+    
+      
+        
+            
 };
-
-
 
 
 function closeTerminal(){
@@ -77,6 +114,7 @@ function closeTerminal(){
     document.getElementById("terminalPopup").style.display = "none";
 
 }
+
 
 
 
